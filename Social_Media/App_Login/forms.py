20ea68034ade42e7812406864bc6c19c2088f7c0
Mia Md Mufrid Shovon <shovonmufrid98@gmail.com>
2023-01-17 17:56:56 +0600

    signup

---
+++ Social_Media/App_Login/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.contrib.auth.models import User
+
+class CreateNewUser(UserCreationForm):
+    email = forms.EmailField(required=True, label='', widget=forms.TextInput(attrs={'placeholder':'Email'}))
+    username= forms.CharField(required=True, label='', widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'password1', 'password2')
